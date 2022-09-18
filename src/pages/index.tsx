fcd67361f5d581ac25e35208ca4e6f7d4c02df8a
benji-devw <bennav.devweb@gmail.com>
2022-09-18 12:16:52 +0200

    Update Index path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Layout from '@/structures/Layout';
-import { HeroFeatures } from '@/features/Hero_Features';
-import { ServicesFeatures } from '@/features/Services_Features';
+import { HeroFeatures } from '@/features/hero_Features';
+import { ServicesFeatures } from '@/features/services_Features';
 import { SkillsFeatures } from '@/features/Skills_Features';
 import { PortfolioFeatures } from '@/features/Portfolio_Features';
 import { WebAreaFeatures } from '@/features/WebArea_Features';
